fdd40eed23939971bab367f81ea2d43e44d397c9
bos-hieu <12520839@gm.uit.edu.vn>
2024-04-03 00:21:02 -0600

    feat: add contract

---
+++ src/components/Product.js
@@ -0,0 +1,31 @@
+export default function Product({ product }) {
+    const { id, name, description, price, image } = product
+    const ethDecimal = 1e18;
+    const priceDisplay = price / ethDecimal;
+    // change "image 1" to image_1
+    const beautyImage = image.replace(/\s/g, "").toLowerCase();
+
+    return (
+        <div
+            className="max-w-[250px] rounded overflow-hidden shadow-lg"
+            key={id}
+        >
+            <img
+                className="w-full"
+                width={250}
+                height={250}
+                // objectFit="cover"
+                // %20 is a space
+                src={`/images/${beautyImage}.jpg`}
+                alt={name}
+            />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{name}</div>
+                <p className="text-gray-700 text-base">{description}</p>
+                <p className="text-gray-900 text-xl">{priceDisplay} ETH</p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+            </div>
+        </div>
+    )
+}
