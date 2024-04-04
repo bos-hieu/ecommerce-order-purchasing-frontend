2be28311fe842f7ef63c3d11df16cfc86935cf29
bos-hieu <12520839@gm.uit.edu.vn>
2024-04-04 14:15:40 -0600

    feat: remove unused

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,11 +7,9 @@ export default function Product({product, placeOrder}) {
 
     return (
         <div
-            className="max-w-[250px] rounded overflow-hidden shadow-lg"
             key={id}
         >
             <img
-                className="w-full"
                 width={250}
                 height={250}
                 // objectFit="cover"
@@ -19,18 +17,17 @@ export default function Product({product, placeOrder}) {
                 src={`/images/${beautyImage}.jpg`}
                 alt={name}
             />
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{name}</div>
-                <p className="text-gray-700 text-base">{description}</p>
-                <p className="text-gray-900 text-xl">{priceDisplay} ETH</p>
+            <div>
+                <div>{name}</div>
+                <p>{description}</p>
+                <p>{priceDisplay} ETH</p>
                 <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     onClick={() => placeOrder(id, price)}
                 >
                     Place Order
                 </button>
             </div>
-            <div className="px-6 pt-4 pb-2">
+            <div>
             </div>
         </div>
     )
